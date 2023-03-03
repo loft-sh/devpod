d014115b925481a1ec5b02935aa8408b3f70ee73
Fabian Kramm <fab.kramm@googlemail.com>
2023-03-03 17:57:05 +0100

    feat: add devpod provider update command

--- cmd/provider/update.go
+++ cmd/provider/update.go
@@ -10,19 +10,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// AddCmd holds the cmd flags
-type AddCmd struct {
+// UpdateCmd holds the cmd flags
+type UpdateCmd struct {
 	*flags.GlobalFlags
 }
 
-// NewAddCmd creates a new command
-func NewAddCmd(flags *flags.GlobalFlags) *cobra.Command {
-	cmd := &AddCmd{
+// NewUpdateCmd creates a new command
+func NewUpdateCmd(flags *flags.GlobalFlags) *cobra.Command {
+	cmd := &UpdateCmd{
 		GlobalFlags: flags,
 	}
-	addCmd := &cobra.Command{
-		Use:   "add",
-		Short: "Adds a new provider to DevPod",
+	updateCmd := &cobra.Command{
+		Use:   "update",
+		Short: "Updates a provider in DevPod",
 		RunE: func(_ *cobra.Command, args []string) error {
 			ctx := context.Background()
 			devPodConfig, err := config.LoadConfig(cmd.Context)
@@ -34,20 +34,20 @@ func NewAddCmd(flags *flags.GlobalFlags) *cobra.Command {
 		},
 	}
 
-	return addCmd
+	return updateCmd
 }
 
-func (cmd *AddCmd) Run(ctx context.Context, devPodConfig *config.Config, args []string) error {
+func (cmd *UpdateCmd) Run(ctx context.Context, devPodConfig *config.Config, args []string) error {
 	if len(args) != 1 {
-		return fmt.Errorf("please specify either a local file, url or git repository. E.g. devpod provider add https://path/to/my/provider.yaml")
+		return fmt.Errorf("please specify either a local file, url or git repository. E.g. devpod provider update https://path/to/my/provider.yaml")
 	}
 
-	providerConfig, err := workspace.AddProvider(devPodConfig, args[0], log.Default)
+	providerConfig, err := workspace.UpdateProvider(devPodConfig, args[0], log.Default)
 	if err != nil {
 		return err
 	}
 
-	log.Default.Donef("Successfully installed provider %s", providerConfig.Name)
+	log.Default.Donef("Successfully updated provider %s", providerConfig.Name)
 	log.Default.Infof("To use the provider, please run the following command:")
 	log.Default.Infof("devpod use provider %s", providerConfig.Name)
 	return nil
