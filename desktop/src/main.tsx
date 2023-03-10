1eace835c6389128208d777c5a13081fbfe1b3fd
Fabian Kramm <fab.kramm@googlemail.com>
2023-03-10 15:03:58 +0100

    refactor: add xtermjs styling

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode, useEffect } from "react"
 import ReactDOM from "react-dom/client"
 import { App } from "./App"
 import { DevPodProvider } from "./contexts/DevPodContext/DevPodContext"
+import "xterm/css/xterm.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<Root />)
 
