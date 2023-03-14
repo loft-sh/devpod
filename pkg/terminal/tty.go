c6d2fe51619efd23c25bfd26691531a84e962763
Fabian Kramm <fab.kramm@googlemail.com>
2023-03-14 12:55:54 +0100

    refactor: remove k8s dependency

--- pkg/terminal/tty.go
+++ pkg/terminal/tty.go
@@ -5,32 +5,12 @@ import (
 	"os"
 
 	dockerterm "github.com/moby/term"
-	"k8s.io/kubectl/pkg/util/term"
 )
 
-var _, tty = SetupTTY(os.Stdin, os.Stdout)
-var IsTerminalIn = tty.IsTerminalIn()
+var IsTerminalIn = IsTerminal(os.Stdin)
 
-// SetupTTY creates a term.TTY (docker)
-func SetupTTY(stdin io.Reader, stdout io.Writer) (bool, term.TTY) {
-	t := term.TTY{
-		Out: stdout,
-		In:  stdin,
-	}
-
-	if !t.IsTerminalIn() {
-		return false, t
-	}
-
-	// if we get to here, the user wants to attach stdin, wants a TTY, and In is a terminal, so we
-	// can safely set t.Raw to true
-	t.Raw = true
-
-	newStdin, newStdout, _ := dockerterm.StdStreams()
-	t.In = newStdin
-	if stdout != nil {
-		t.Out = newStdout
-	}
-
-	return true, t
+// IsTerminal returns whether the passed object is a terminal or not
+func IsTerminal(stdin io.Reader) bool {
+	_, terminal := dockerterm.GetFdInfo(stdin)
+	return terminal
 }
