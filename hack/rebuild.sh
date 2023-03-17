3f3c5c06e5df873b6f5674c1ccba7a60b0ea7f14
Fabian Kramm <fab.kramm@googlemail.com>
2023-03-17 14:38:17 +0100

    chore: windows build & list version

--- hack/rebuild.sh
+++ hack/rebuild.sh
@@ -6,6 +6,7 @@ GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-linux-amd64
 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-linux-arm64
 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-darwin-arm64
 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-darwin-amd64
+GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-windows-amd64
 sudo go build -o /usr/local/bin/devpod
 cp test/devpod-linux-amd64 desktop/src-tauri/bin/devpod-x86_64-unknown-linux-gnu
 cp test/devpod-linux-arm64 desktop/src-tauri/bin/devpod-aarch64-unknown-linux-gnu
