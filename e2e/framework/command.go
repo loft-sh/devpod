b21c3c54c427bb874bafc6c20ba6de9a7b516e13
Kristof Nagy <kristofn@protonmail.ch>
2023-03-22 21:46:39 +0100

    test(e2e): add provider test cases

--- e2e/framework/command.go
+++ e2e/framework/command.go
@@ -3,6 +3,9 @@ package framework
 import (
 	"context"
 	"fmt"
+
+	"github.com/loft-sh/devpod/cmd/flags"
+	"github.com/loft-sh/devpod/cmd/provider"
 )
 
 // DevPodUp executes the `devpod up` command in the test framework
@@ -22,10 +25,33 @@ func (f *Framework) DevPodSSHEchoTestString(ctx context.Context, workspace strin
 	return nil
 }
 
+func (f *Framework) DevPodProviderOptionsCheckNamespaceDescription(ctx context.Context, provider, searchStr string) error {
+	err := f.ExecCommand(ctx, true, true, searchStr, []string{"provider", "options", provider})
+	if err != nil {
+		return fmt.Errorf("did not found value %s in devpod provider options output. error: %s", searchStr, err.Error())
+	}
+	return nil
+}
+
 func (f *Framework) DevPodProviderUse(ctx context.Context, provider string) error {
 	err := f.ExecCommand(ctx, false, true, "", []string{"provider", "use", provider})
 	if err != nil {
 		return fmt.Errorf("devpod provider use failed: %s", err.Error())
 	}
 	return nil
+}
+
+func (f *Framework) DevPodProviderAdd(args []string) error {
+	addCmd := provider.NewAddCmd(&flags.GlobalFlags{})
+	return addCmd.RunE(nil, args)
+}
+
+func (f *Framework) DevPodProviderDelete(args []string) error {
+	deleteCmd := provider.NewDeleteCmd(&flags.GlobalFlags{})
+	return deleteCmd.RunE(nil, args)
+}
+
+func (f *Framework) DevPodProviderUpdate(args []string) error {
+	updateCmd := provider.NewUpdateCmd(&flags.GlobalFlags{})
+	return updateCmd.RunE(nil, args)
 }
