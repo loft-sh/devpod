55eb99ea8c47fbf6a6bfad9f18fc23a8f0359922
Fabian Kramm <fab.kramm@googlemail.com>
2023-03-23 19:07:19 +0100

    feat: add get name commands

---
+++ cmd/helper/get_workspace_name.go
@@ -0,0 +1,38 @@
+package helper
+
+import (
+	"context"
+	"fmt"
+	"github.com/loft-sh/devpod/cmd/flags"
+	"github.com/loft-sh/devpod/pkg/workspace"
+	"github.com/spf13/cobra"
+)
+
+type GetWorkspaceNameCommand struct {
+	*flags.GlobalFlags
+}
+
+// NewGetWorkspaceNameCmd creates a new command
+func NewGetWorkspaceNameCmd(flags *flags.GlobalFlags) *cobra.Command {
+	cmd := &GetWorkspaceNameCommand{
+		GlobalFlags: flags,
+	}
+	shellCmd := &cobra.Command{
+		Use:   "get-workspace-name",
+		Short: "Retrieves a workspace name",
+		RunE: func(_ *cobra.Command, args []string) error {
+			return cmd.Run(context.Background(), args)
+		},
+	}
+
+	return shellCmd
+}
+
+func (cmd *GetWorkspaceNameCommand) Run(ctx context.Context, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("workspace is missing")
+	}
+
+	fmt.Print(workspace.GetWorkspaceName(args))
+	return nil
+}
