4656e564f8da0ddfb38277c959f02bce3f79696e
Pascal Breuninger <pascal.breuninger@loft.sh>
2023-04-17 07:50:30 +0200

    chore(ui): setup release pipeline

--- hack/build-desktop-cli.bat
+++ hack/build-desktop-cli.bat
@@ -3,6 +3,10 @@
 set GOOS=windows
 set GOARCH=amd64
 
-go build -ldflags "-s -w" -o test/devpod-windows-amd64
+go build -ldflags "-s -w" -o test/devpod-x86_64-pc-windows-msvc.exe
 
-xcopy /F /Y test\devpod-windows-amd64 desktop\bin\*
+set GOARCH=arm64
+go build -ldflags "-s -w" -o test/devpod-aarch64-pc-windows-msvc.exe
+
+xcopy /F /Y test\devpod-x86_64-pc-windows-msvc.exe desktop\bin\*
+xcopy /F /Y test\devpod-aarch64-pc-windows-msvc.exe desktop\bin\*
--- hack/build-desktop-cli.sh
+++ hack/build-desktop-cli.sh
@@ -2,17 +2,19 @@
 
 set -e
 
+# Linux
 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-linux-amd64
 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-linux-arm64
-GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-darwin-arm64
-GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-darwin-amd64 
-GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-windows-amd64
 cp test/devpod-linux-amd64 desktop/src-tauri/bin/devpod-x86_64-unknown-linux-gnu
 cp test/devpod-linux-arm64 desktop/src-tauri/bin/devpod-aarch64-unknown-linux-gnu
+
+# MacOS
+GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-darwin-arm64
+GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-darwin-amd64 
 cp test/devpod-darwin-amd64 desktop/src-tauri/bin/devpod-x86_64-apple-darwin
 cp test/devpod-darwin-arm64 desktop/src-tauri/bin/devpod-aarch64-apple-darwin
 
-if [[ -z "${GITHUB_TOKEN}" ]]; then
+if [[ -z "${CI}" ]]; then
     CACHE=$TMPDIR/devpod-cache
     [ ! -e $CACHE ] || rm -R $CACHE 
 fi
