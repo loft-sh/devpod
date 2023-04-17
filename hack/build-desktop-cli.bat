f8ccc7af60bd14d9e510b7aa73f4dcd54adf165e
Pascal Breuninger <117151367+pascalbreuninger@users.noreply.github.com>
2023-04-17 16:42:38 +0200

    chore(ui): setup release pipeline (#130)

--- hack/build-desktop-cli.bat
+++ hack/build-desktop-cli.bat
@@ -5,8 +5,4 @@ set GOARCH=amd64
 
 go build -ldflags "-s -w" -o test/devpod-x86_64-pc-windows-msvc.exe
 
-set GOARCH=arm64
-go build -ldflags "-s -w" -o test/devpod-aarch64-pc-windows-msvc.exe
-
 xcopy /F /Y test\devpod-x86_64-pc-windows-msvc.exe desktop\bin\*
-xcopy /F /Y test\devpod-aarch64-pc-windows-msvc.exe desktop\bin\*
