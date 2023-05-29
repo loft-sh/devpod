029c7821e946bd69358cb93d2030393a082116a7
pleclech <pleclech@users.noreply.github.com>
2023-05-29 16:26:04 +0200

    index should be decremented
    I just fall on this bug today with  fatal agent error: panic: runtime error: index out of range [2] with length 2 goroutine 1.
    
    And after investigating found that [i](https://github.com/loft-sh/devpod/blob/204d0decc41768ff5494c396462220c948d14995/pkg/dockerfile/parse.go#L141) is incremented when it should bedecremented,

--- pkg/dockerfile/parse.go
+++ pkg/dockerfile/parse.go
@@ -138,7 +138,7 @@ func (d *Dockerfile) findValue(buildArgs map[string]string, baseImageEnv map[str
 		}
 
 		// search env
-		for i := len(stage.Envs) - 1; i >= 0; i++ {
+		for i := len(stage.Envs) - 1; i >= 0; i-- {
 			if stage.Envs[i].Key != variable || stage.Envs[i].Line >= untilLine {
 				continue
 			}
