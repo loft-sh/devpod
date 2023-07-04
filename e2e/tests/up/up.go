a4d72924c1977ee2a81333ef14c76d6187208394
Luca Di Maio <luca.dimaio1@gmail.com>
2023-07-04 13:58:30 +0200

    tests: fix errors in adding existing providers
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- e2e/tests/up/up.go
+++ e2e/tests/up/up.go
@@ -73,14 +73,41 @@ var _ = DevPodDescribe("devpod up test suite", func() {
 	})
 
 	ginkgo.Context("using docker provider", func() {
-		ginkgo.Context("with podman", func() {
+		ginkgo.Context("with rootfull podman", ginkgo.Ordered, func() {
+			ginkgo.It("should setup rootful podman", func(ctx context.Context) {
+				wrapper, err := os.Create(initialDir + "/bin/podman-rootful")
+				framework.ExpectNoError(err)
+
+				defer wrapper.Close()
+
+				_, err = wrapper.WriteString(`#!/bin/sh
+				sudo podman "$@"
+				`)
+				framework.ExpectNoError(err)
+
+				err = wrapper.Close()
+				framework.ExpectNoError(err)
+
+				cmd := exec.Command("sudo", "chmod", "+x", initialDir+"/bin/podman-rootful")
+				err = cmd.Run()
+				framework.ExpectNoError(err)
+
+				err = exec.Command(initialDir+"/bin/podman-rootful", "ps").Run()
+				framework.ExpectNoError(err)
+
+			}, ginkgo.SpecTimeout(60*time.Second))
+
 			ginkgo.It("should start a new workspace with existing image", func(ctx context.Context) {
 				tempDir, err := framework.CopyToTempDir("tests/up/testdata/docker")
 				framework.ExpectNoError(err)
 				ginkgo.DeferCleanup(framework.CleanupTempDir, initialDir, tempDir)
 
 				f := framework.NewDefaultFramework(initialDir + "/bin")
-				_ = f.DevPodProviderAdd([]string{"docker", "-o", "DOCKER_PATH=podman"})
+
+				_ = f.DevPodProviderDelete([]string{"docker"})
+				err = f.DevPodProviderAdd([]string{"docker", "-o", "DOCKER_PATH=" + initialDir + "/bin/podman-rootful"})
+				framework.ExpectNoError(err)
+
 				err = f.DevPodProviderUse(context.Background(), "docker")
 				framework.ExpectNoError(err)
 
@@ -88,30 +115,38 @@ var _ = DevPodDescribe("devpod up test suite", func() {
 				err = f.DevPodUp(ctx, tempDir)
 				framework.ExpectNoError(err)
 			}, ginkgo.SpecTimeout(60*time.Second))
-
-			ginkgo.It("should start a new workspace with multistage build", func(ctx context.Context) {
-				tempDir, err := framework.CopyToTempDir("tests/up/testdata/docker-with-multi-stage-build")
-				framework.ExpectNoError(err)
-				ginkgo.DeferCleanup(framework.CleanupTempDir, initialDir, tempDir)
-
-				f := framework.NewDefaultFramework(initialDir + "/bin")
-				_ = f.DevPodProviderAdd([]string{"docker", "-o", "DOCKER_PATH=podman"})
-				err = f.DevPodProviderUse(context.Background(), "docker")
-				framework.ExpectNoError(err)
-
-				// Wait for devpod workspace to come online (deadline: 30s)
-				err = f.DevPodUp(ctx, tempDir, "--debug")
-				framework.ExpectNoError(err)
-			}, ginkgo.SpecTimeout(60*time.Second))
 		})
-		ginkgo.Context("with docker", func() {
+		ginkgo.Context("with rootless podman", ginkgo.Ordered, func() {
 			ginkgo.It("should start a new workspace with existing image", func(ctx context.Context) {
 				tempDir, err := framework.CopyToTempDir("tests/up/testdata/docker")
 				framework.ExpectNoError(err)
 				ginkgo.DeferCleanup(framework.CleanupTempDir, initialDir, tempDir)
 
 				f := framework.NewDefaultFramework(initialDir + "/bin")
-				_ = f.DevPodProviderAdd([]string{"docker"})
+
+				_ = f.DevPodProviderDelete([]string{"docker"})
+				err = f.DevPodProviderAdd([]string{"docker", "-o", "DOCKER_PATH=podman"})
+				framework.ExpectNoError(err)
+				err = f.DevPodProviderUse(context.Background(), "docker")
+				framework.ExpectNoError(err)
+
+				// Wait for devpod workspace to come online (deadline: 30s)
+				err = f.DevPodUp(ctx, tempDir)
+				framework.ExpectNoError(err)
+			}, ginkgo.SpecTimeout(60*time.Second))
+		})
+
+		ginkgo.Context("with docker", ginkgo.Ordered, func() {
+			ginkgo.It("should start a new workspace with existing image", func(ctx context.Context) {
+				tempDir, err := framework.CopyToTempDir("tests/up/testdata/docker")
+				framework.ExpectNoError(err)
+				ginkgo.DeferCleanup(framework.CleanupTempDir, initialDir, tempDir)
+
+				f := framework.NewDefaultFramework(initialDir + "/bin")
+
+				_ = f.DevPodProviderDelete([]string{"docker"})
+				err = f.DevPodProviderAdd([]string{"docker"})
+				framework.ExpectNoError(err)
 				err = f.DevPodProviderUse(context.Background(), "docker")
 				framework.ExpectNoError(err)
 
