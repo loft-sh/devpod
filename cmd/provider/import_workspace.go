a8c27af3a247d18977dc9c339b78b39d8338ab2f
Przemysław Białoń <przem0.b95@gmail.com>
2023-08-14 12:21:22 +0200

    fixed provider fetching based on providerID

--- cmd/provider/import_workspace.go
+++ cmd/provider/import_workspace.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"github.com/loft-sh/devpod/cmd/flags"
@@ -9,6 +8,7 @@ import (
 	"github.com/loft-sh/devpod/pkg/client/clientimplementation"
 	"github.com/loft-sh/devpod/pkg/config"
 	provider2 "github.com/loft-sh/devpod/pkg/provider"
+	"github.com/loft-sh/devpod/pkg/workspace"
 	"github.com/loft-sh/log"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -41,17 +41,16 @@ func (cmd *ImportCmd) Import(ctx context.Context, args []string) error {
 	if err != nil {
 		return err
 	}
-
-	proxyProvider, err := cmd.getProxyProvider(workspaceMetaData.ProviderID)
+	devPodConfig, err := config.LoadConfig(cmd.Context, workspaceMetaData.ProviderID)
+	proxyProvider, err := cmd.getProxyProvider(devPodConfig, workspaceMetaData.ProviderID)
 	if err != nil {
 		return err
 	}
-	devPodConfig, err := config.LoadConfig(cmd.Context, proxyProvider.Name)
-	workspace := &provider2.Workspace{
+	w := &provider2.Workspace{
 		//todo: fill workspace data
 	}
 
-	workspaceClient, err := clientimplementation.NewProxyClient(devPodConfig, proxyProvider, workspace, cmd.log)
+	workspaceClient, err := clientimplementation.NewProxyClient(devPodConfig, proxyProvider, w, cmd.log)
 	if err != nil {
 		return err
 	}
@@ -64,17 +63,17 @@ type WorkspaceMetaData struct {
 	ProviderID   string `json:"providerID"`
 }
 
-func (cmd *ImportCmd) getProxyProvider(providerID string) (*provider2.ProviderConfig, error) {
-	provider, err := provider2.ParseProvider(bytes.NewReader([]byte(providerID)))
+func (cmd *ImportCmd) getProxyProvider(devpodConfig *config.Config, providerID string) (*provider2.ProviderConfig, error) {
+	provider, err := workspace.FindProvider(devpodConfig, providerID, cmd.log)
 	if err != nil {
-		return nil, errors.Wrap(err, "parse provider")
+		return nil, errors.Wrap(err, "find provider")
 	}
 
-	if !provider.IsProxyProvider() {
+	if !provider.Config.IsProxyProvider() {
 		return nil, fmt.Errorf("provider is not a proxy provider")
 	}
 
-	return provider, nil
+	return provider.Config, nil
 }
 
 func (cmd *ImportCmd) readWorkspaceMetaData() (*WorkspaceMetaData, error) {
