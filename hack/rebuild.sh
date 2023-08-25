b7be618aad97d99f7b129fd6e3822ed5c7b1e979
Przemysław Białoń <przem0.b95@gmail.com>
2023-08-25 13:53:11 +0200

    Added logging to rebuild.sh script

--- hack/rebuild.sh
+++ hack/rebuild.sh
@@ -7,15 +7,19 @@ CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-c
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o test/devpod-cli-darwin-arm64
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-cli-darwin-amd64
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o test/devpod-cli-windows-amd64
+echo "[INFO] Built binaries for all platforms in test/ directory"
 go build -o test/devpod && sudo mv test/devpod /usr/local/bin/
+echo "[INFO] Built devpod binary and moved to /usr/local/bin"
 cp test/devpod-cli-linux-amd64 test/devpod-linux-amd64
 cp test/devpod-cli-linux-arm64 test/devpod-linux-arm64
 cp test/devpod-cli-linux-amd64 desktop/src-tauri/bin/devpod-cli-x86_64-unknown-linux-gnu
 cp test/devpod-cli-linux-arm64 desktop/src-tauri/bin/devpod-cli-aarch64-unknown-linux-gnu
 cp test/devpod-cli-darwin-amd64 desktop/src-tauri/bin/devpod-cli-x86_64-apple-darwin
 cp test/devpod-cli-darwin-arm64 desktop/src-tauri/bin/devpod-cli-aarch64-apple-darwin
+echo "[INFO] Copied binaries to desktop/src-tauri/bin"
 
 rm -R $TMPDIR/devpod-cache 2>/dev/null || true
 mkdir -p $TMPDIR/devpod-cache
 cp test/devpod-cli-linux-amd64 $TMPDIR/devpod-cache/devpod-linux-amd64
 cp test/devpod-cli-linux-arm64 $TMPDIR/devpod-cache/devpod-linux-arm64
+echo "[INFO] Copied binaries to $TMPDIR/devpod-cache"
