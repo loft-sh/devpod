47e53c52e8c9ad4c1d6ba2e80573bbb55a74870f
neogopher <sachin.lobo@infracloud.io>
2023-08-31 12:46:29 +0530

    Add tests

--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -8,86 +8,136 @@ import (
 )
 
 type testCaseNormalizeRepository struct {
+	in                  string
+	expectedPRReference string
+	expectedRepo        string
+	expectedBranch      string
+	expectedCommit      string
+}
+
+type testCaseGetBranchNameForPR struct {
 	in             string
-	expectedRepo   string
 	expectedBranch string
-	expectedCommit string
 }
 
 func TestNormalizeRepository(t *testing.T) {
 	testCases := []testCaseNormalizeRepository{
 		{
-			in:             "ssh://github.com/loft-sh/devpod.git",
-			expectedRepo:   "ssh://github.com/loft-sh/devpod.git",
-			expectedBranch: "",
-			expectedCommit: "",
+			in:                  "ssh://github.com/loft-sh/devpod.git",
+			expectedRepo:        "ssh://github.com/loft-sh/devpod.git",
+			expectedPRReference: "",
+			expectedBranch:      "",
+			expectedCommit:      "",
 		},
 		{
-			in:             "git@github.com/loft-sh/devpod-without-branch.git",
-			expectedRepo:   "git@github.com/loft-sh/devpod-without-branch.git",
-			expectedBranch: "",
-			expectedCommit: "",
+			in:                  "git@github.com/loft-sh/devpod-without-branch.git",
+			expectedRepo:        "git@github.com/loft-sh/devpod-without-branch.git",
+			expectedPRReference: "",
+			expectedBranch:      "",
+			expectedCommit:      "",
 		},
 		{
-			in:             "https://github.com/loft-sh/devpod.git",
-			expectedRepo:   "https://github.com/loft-sh/devpod.git",
-			expectedBranch: "",
-			expectedCommit: "",
+			in:                  "https://github.com/loft-sh/devpod.git",
+			expectedRepo:        "https://github.com/loft-sh/devpod.git",
+			expectedPRReference: "",
+			expectedBranch:      "",
+			expectedCommit:      "",
 		},
 		{
-			in:             "github.com/loft-sh/devpod.git",
-			expectedRepo:   "https://github.com/loft-sh/devpod.git",
-			expectedBranch: "",
-			expectedCommit: "",
+			in:                  "github.com/loft-sh/devpod.git",
+			expectedRepo:        "https://github.com/loft-sh/devpod.git",
+			expectedPRReference: "",
+			expectedBranch:      "",
+			expectedCommit:      "",
 		},
 		{
-			in:             "github.com/loft-sh/devpod.git@test-branch",
-			expectedRepo:   "https://github.com/loft-sh/devpod.git",
-			expectedBranch: "test-branch",
-			expectedCommit: "",
+			in:                  "github.com/loft-sh/devpod.git@test-branch",
+			expectedRepo:        "https://github.com/loft-sh/devpod.git",
+			expectedPRReference: "",
+			expectedBranch:      "test-branch",
+			expectedCommit:      "",
 		},
 		{
-			in:             "git@github.com/loft-sh/devpod-with-branch.git@test-branch",
-			expectedRepo:   "git@github.com/loft-sh/devpod-with-branch.git",
-			expectedBranch: "test-branch",
-			expectedCommit: "",
+			in:                  "git@github.com/loft-sh/devpod-with-branch.git@test-branch",
+			expectedRepo:        "git@github.com/loft-sh/devpod-with-branch.git",
+			expectedPRReference: "",
+			expectedBranch:      "test-branch",
+			expectedCommit:      "",
 		},
 		{
-			in:             "git@github.com/loft-sh/devpod-with-branch.git@test_branch",
-			expectedRepo:   "git@github.com/loft-sh/devpod-with-branch.git",
-			expectedBranch: "test_branch",
-			expectedCommit: "",
+			in:                  "git@github.com/loft-sh/devpod-with-branch.git@test_branch",
+			expectedRepo:        "git@github.com/loft-sh/devpod-with-branch.git",
+			expectedPRReference: "",
+			expectedBranch:      "test_branch",
+			expectedCommit:      "",
 		},
 		{
-			in:             "github.com/loft-sh/devpod-without-protocol-with-slash.git@user/branch",
-			expectedRepo:   "https://github.com/loft-sh/devpod-without-protocol-with-slash.git",
-			expectedBranch: "user/branch",
-			expectedCommit: "",
+			in:                  "github.com/loft-sh/devpod-without-protocol-with-slash.git@user/branch",
+			expectedRepo:        "https://github.com/loft-sh/devpod-without-protocol-with-slash.git",
+			expectedPRReference: "",
+			expectedBranch:      "user/branch",
+			expectedCommit:      "",
 		},
 		{
-			in:             "git@github.com/loft-sh/devpod-with-slash.git@user/branch",
-			expectedRepo:   "git@github.com/loft-sh/devpod-with-slash.git",
-			expectedBranch: "user/branch",
-			expectedCommit: "",
+			in:                  "git@github.com/loft-sh/devpod-with-slash.git@user/branch",
+			expectedRepo:        "git@github.com/loft-sh/devpod-with-slash.git",
+			expectedPRReference: "",
+			expectedBranch:      "user/branch",
+			expectedCommit:      "",
 		},
 		{
-			in:             "github.com/loft-sh/devpod.git@sha256:905ffb0",
-			expectedRepo:   "https://github.com/loft-sh/devpod.git",
-			expectedBranch: "",
-			expectedCommit: "905ffb0",
+			in:                  "github.com/loft-sh/devpod.git@sha256:905ffb0",
+			expectedRepo:        "https://github.com/loft-sh/devpod.git",
+			expectedPRReference: "",
+			expectedBranch:      "",
+			expectedCommit:      "905ffb0",
 		},
 		{
-			in:             "git@github.com:loft-sh/devpod.git@sha256:905ffb0",
-			expectedRepo:   "git@github.com:loft-sh/devpod.git",
-			expectedBranch: "",
-			expectedCommit: "905ffb0",
+			in:                  "git@github.com:loft-sh/devpod.git@sha256:905ffb0",
+			expectedRepo:        "git@github.com:loft-sh/devpod.git",
+			expectedPRReference: "",
+			expectedBranch:      "",
+			expectedCommit:      "905ffb0",
+		},
+		{
+			in:                  "github.com/loft-sh/devpod.git@pull/996/head",
+			expectedRepo:        "https://github.com/loft-sh/devpod.git",
+			expectedPRReference: "pull/996/head",
+			expectedBranch:      "",
+			expectedCommit:      "",
+		},
+		{
+			in:                  "git@github.com:loft-sh/devpod.git@pull/996/head",
+			expectedRepo:        "git@github.com:loft-sh/devpod.git",
+			expectedPRReference: "pull/996/head",
+			expectedBranch:      "",
+			expectedCommit:      "",
 		},
 	}
 
 	for _, testCase := range testCases {
-		outRepo, outBranch, outCommit := NormalizeRepository(testCase.in)
+		outRepo, outPRReference, outBranch, outCommit := NormalizeRepository(testCase.in)
 		assert.Check(t, cmp.Equal(testCase.expectedRepo, outRepo))
+		assert.Check(t, cmp.Equal(testCase.expectedPRReference, outPRReference))
 		assert.Check(t, cmp.Equal(testCase.expectedBranch, outBranch))
 		assert.Check(t, cmp.Equal(testCase.expectedCommit, outCommit))
+	}
+}
+
+func TestGetBranchNameForPRReference(t *testing.T) {
+	testCases := []testCaseGetBranchNameForPR{
+		{
+			in:             "pull/996/head",
+			expectedBranch: "PR996",
+		},
+		{
+			in:             "pull/abc/head",
+			expectedBranch: "pull/abc/head",
+		},
+	}
+
+	for _, testCase := range testCases {
+		outBranch := GetBranchNameForPR(testCase.in)
+		assert.Check(t, cmp.Equal(testCase.expectedBranch, outBranch))
 	}
 }
