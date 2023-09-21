55d22603f2470f2224d83e924776fea79efd22c2
Luca Di Maio <luca.dimaio1@gmail.com>
2023-09-21 16:18:19 +0200

    fix: unstuck ssh commands on different systems
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -385,7 +385,7 @@ func (cmd *SSHCmd) startTunnel(
 
 		// capture the output, if it's empty it means we don't have gpg-forwarding
 		var out bytes.Buffer
-		err := devssh.Run(ctx, containerClient, command, os.Stdin, &out, writer)
+		err := devssh.Run(ctx, containerClient, command, nil, &out, writer)
 
 		if err == nil && len(out.Bytes()) > 1 {
 			log.Debugf("gpg: exporting already running, skipping")
