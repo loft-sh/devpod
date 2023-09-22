1754f8f11a1c6bf80bd94521997dde19298744bf
Przemysław Białoń <przem0.b95@gmail.com>
2023-09-22 15:00:11 +0200

    fail fast if mounted filesystem for agent is noexec

--- pkg/inject/inject.go
+++ pkg/inject/inject.go
@@ -31,6 +31,16 @@ type injectResult struct {
 	err         error
 }
 
+type NoexecFilesystemError struct {
+	msg string
+}
+
+func (e NoexecFilesystemError) Error() string {
+	return e.msg
+}
+
+var ErrNoExecFilesystem = NoexecFilesystemError{msg: "filesystem is noexec"}
+
 func InjectAndExecute(
 	ctx context.Context,
 	exec ExecFunc,
@@ -63,6 +73,15 @@ func InjectAndExecute(
 	})
 	if err != nil {
 		return true, err
+	}
+
+	log.Debugf("ensure agent path's filesystem is not noexec")
+	noExec, err := isNoExec(remotePath)
+	if err != nil {
+		return false, perrors.Wrapf(err, "check if filesystem is noexec")
+	}
+	if noExec {
+		return false, ErrNoExecFilesystem
 	}
 
 	log.Debugf("execute inject script")
