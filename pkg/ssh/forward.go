755f12b6199899f339bffce09410df129f15b941
Luca Di Maio <luca.dimaio1@gmail.com>
2023-09-26 09:53:44 +0200

    fix: deduplicate coding between Forward and ReverseForward
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- pkg/ssh/forward.go
+++ pkg/ssh/forward.go
@@ -11,6 +11,14 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+type ForwardingFunction func(
+	net.Conn,
+	*ssh.Client,
+	string,
+	string,
+	log.Logger,
+)
+
 func PortForward(
 	ctx context.Context,
 	client *ssh.Client,
@@ -24,6 +32,42 @@ func PortForward(
 	}
 	defer listener.Close()
 
+	return portForwarding(
+		ctx, client, listener,
+		localNetwork, localAddr, remoteNetwork, remoteAddr,
+		exitAfterTimeout, log, forward,
+	)
+}
+
+func ReversePortForward(
+	ctx context.Context,
+	client *ssh.Client,
+	remoteNetwork, remoteAddr, localNetwork, localAddr string,
+	exitAfterTimeout time.Duration,
+	log log.Logger,
+) error {
+	listener, err := client.Listen(remoteNetwork, remoteAddr)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	return portForwarding(
+		ctx, client, listener,
+		remoteNetwork, remoteAddr, localNetwork, localAddr,
+		exitAfterTimeout, log, reverseForward,
+	)
+}
+
+func portForwarding(
+	ctx context.Context,
+	client *ssh.Client,
+	listener net.Listener,
+	srcNetwork, srcAddr, dstNetwork, dstAddr string,
+	exitAfterTimeout time.Duration,
+	log log.Logger,
+	forwardFn ForwardingFunction,
+) error {
 	done := make(chan struct{})
 	defer close(done)
 
@@ -37,10 +81,10 @@ func PortForward(
 
 	counter := newConnectionCounter(ctx, exitAfterTimeout, func() {
 		log.Fatal("Stopping devpod up, because it stayed idle for a while. You can disable this via 'devpod context set-options -o EXIT_AFTER_TIMEOUT=false'")
-	}, localAddr, log)
+	}, srcAddr, log)
 	for {
 		// waiting for a new connection
-		local, err := listener.Accept()
+		connection, err := listener.Accept()
 		if err != nil {
 			return err
 		}
@@ -52,7 +96,7 @@ func PortForward(
 		go func() {
 			defer counter.Dec()
 
-			forward(local, client, remoteNetwork, remoteAddr, log)
+			forwardFn(connection, client, dstNetwork, dstAddr, log)
 		}()
 	}
 }
@@ -98,53 +142,12 @@ func forward(
 	waitGroup.Wait()
 }
 
-func ReversePortForward(
-	ctx context.Context,
+func reverseForward(
+	remoteConn net.Conn,
 	client *ssh.Client,
-	remoteNetwork, remoteAddr, localNetwork, localAddr string,
-	exitAfterTimeout time.Duration,
+	localNetwork, localAddr string,
 	log log.Logger,
-) error {
-	listener, err := client.Listen(remoteNetwork, remoteAddr)
-	if err != nil {
-		return err
-	}
-	defer listener.Close()
-
-	done := make(chan struct{})
-	defer close(done)
-
-	go func() {
-		select {
-		case <-done:
-		case <-ctx.Done():
-			_ = listener.Close()
-		}
-	}()
-
-	counter := newConnectionCounter(ctx, exitAfterTimeout, func() {
-		log.Fatal("Stopping devpod up, because it stayed idle for a while. You can disable this via 'devpod context set-options -o EXIT_AFTER_TIMEOUT=false'")
-	}, remoteAddr, log)
-	for {
-		// waiting for a new connection
-		remote, err := listener.Accept()
-		if err != nil {
-			return err
-		}
-
-		// tell the counter there is a connection
-		counter.Add()
-
-		// forward connection
-		go func() {
-			defer counter.Dec()
-
-			reverseForward(remote, localNetwork, localAddr, log)
-		}()
-	}
-}
-
-func reverseForward(remoteConn net.Conn, localNetwork, localAddr string, log log.Logger) {
+) {
 	// Setup localConn (type net.Conn)
 	localConn, err := net.Dial(localNetwork, localAddr)
 	if err != nil {
