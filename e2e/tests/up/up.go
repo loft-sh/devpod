4f8be16fb7f4a7ecfe69142a8aec6c32d5053fe4
Luca Di Maio <luca.dimaio1@gmail.com>
2023-10-30 14:26:51 +0100

    test: revert 83024ff98c3d4c93ac244df369f9d9267ee34273
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- e2e/tests/up/up.go
+++ e2e/tests/up/up.go
@@ -269,7 +269,7 @@ var _ = DevPodDescribe("devpod up test suite", func() {
 			framework.ExpectError(err, "expected error")
 			framework.ExpectNoError(verifyLogStream(strings.NewReader(stdout)))
 			framework.ExpectNoError(verifyLogStream(strings.NewReader(stderr)))
-			framework.ExpectNoError(findMessage(strings.NewReader(stderr), "exec: \"abc\": executable file not found in $PATH"))
+			framework.ExpectNoError(findMessage(strings.NewReader(stdout), "exec: \"abc\": executable file not found in $PATH"))
 		}, ginkgo.SpecTimeout(60*time.Second))
 	})
 
