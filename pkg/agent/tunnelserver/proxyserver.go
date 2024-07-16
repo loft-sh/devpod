450be13bd92e3ddd8db707e99f8c8ef5bbfe705f
Jan Baraniewski <dev@baraniewski.com>
2024-07-16 12:34:16 +0200

    Add gitsshsignature implementation to proxyserver

--- pkg/agent/tunnelserver/proxyserver.go
+++ pkg/agent/tunnelserver/proxyserver.go
@@ -93,6 +93,10 @@ func (t *proxyServer) GitCredentials(ctx context.Context, message *tunnel.Messag
 	return t.client.GitCredentials(ctx, message)
 }
 
+func (t *proxyServer) GitSSHSignature(ctx context.Context, message *tunnel.Message) (*tunnel.Message, error) {
+	return t.client.GitSSHSignature(ctx, message)
+}
+
 func (t *proxyServer) SendResult(ctx context.Context, result *tunnel.Message) (*tunnel.Empty, error) {
 	parsedResult := &config.Result{}
 	err := json.Unmarshal([]byte(result.Message), parsedResult)
