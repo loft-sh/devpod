0a979244765def4a4b9c1e7e5422669cc1743191
Jan Baraniewski <dev@baraniewski.com>
2024-07-16 13:59:56 +0200

    Fix: set default scheme for resolver in tunnelclient

--- pkg/agent/tunnelserver/client.go
+++ pkg/agent/tunnelserver/client.go
@@ -9,10 +9,15 @@ import (
 	"github.com/loft-sh/devpod/pkg/stdio"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
 )
 
 func NewTunnelClient(reader io.Reader, writer io.WriteCloser, exitOnClose bool, exitCode int) (tunnel.TunnelClient, error) {
 	pipe := stdio.NewStdioStream(reader, writer, exitOnClose, exitCode)
+
+	// After moving from deprecated grpc.Dial to grpc.NewClient we need to setup resolver first
+	// https://github.com/grpc/grpc-go/issues/1786#issuecomment-2119088770
+	resolver.SetDefaultScheme("passthrough")
 
 	// Set up a connection to the server.
 	conn, err := grpc.NewClient("",
