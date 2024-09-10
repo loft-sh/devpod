6e9d9f2e8c52d41db4fb9b7de767c1dd30b2d9e6
Bryan Kneis <bryan.kneis@loft.sh>
2024-09-10 11:25:30 +0100

    Move advance guide to tutorials

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -190,17 +190,7 @@ module.exports = {
         {
           type: "doc",
           id: "other-topics/mobile-support",
-        },
-        {
-          type: "category",
-          label: "Advanced guides",
-          items: [
-            {
-              type: "doc",
-              id: "other-topics/advanced-guides/minikube-vscode-browser",
-            },
-          ],
-        },
+        }
       ],
     },
     {
