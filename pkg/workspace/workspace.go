3f44e055dfaab6ae82c77c0d17d69023ffb9fde2
Bryan Kneis <bryan.kneis@loft.sh>
2024-09-16 09:33:41 +0100

    Fix PR references

--- pkg/workspace/workspace.go
+++ pkg/workspace/workspace.go
@@ -583,16 +583,17 @@ var (
 func ToID(str string) string {
 	str = strings.ToLower(filepath.ToSlash(str))
 	splitted := strings.Split(str, "@")
-
-	// 1. Check if PR was specified
-	if prReferenceRegEx.MatchString(str) {
-		str = prReferenceRegEx.ReplaceAllStringFunc(str, git.GetBranchNameForPR)
-	} else if len(splitted) == 2 {
-		// 2. Check if a branch name has been specified, if so use this for the ID
-		str = strings.TrimSuffix(splitted[1], ".git")
-		// Check if branch name matches expected regex
-		if !branchRegEx.MatchString(str) {
-			str = splitted[0]
+	if len(splitted) == 2 {
+		// 1. Check if PR was specified
+		if prReferenceRegEx.MatchString(str) {
+			str = prReferenceRegEx.ReplaceAllStringFunc(splitted[1], git.GetBranchNameForPR)
+		} else {
+			// 2. Check if a branch name has been specified, if so use this for the ID
+			str = strings.TrimSuffix(splitted[1], ".git")
+			// Check if branch name matches expected regex
+			if !branchRegEx.MatchString(str) {
+				str = splitted[0]
+			}
 		}
 	} else {
 		// 3. If not, then parse the repo name as ID
