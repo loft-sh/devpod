e7af6ebc453654643cdd3ef35aebc70068a60f7d
Bryan Kneis <bryan.kneis@loft.sh>
2024-11-22 10:14:04 +0000

    Update monitor to get go routine and threads

--- loadtest/init_monitor.sh
+++ loadtest/init_monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-kubectl -n devpod-pro set env deployment/loft LOFTDEBUG=true
+#kubectl -n devpod-pro set env deployment/loft LOFTDEBUG=true
 
-kubectl -n devpod-pro port-forward loft-55df4d875f-gd5tw 8080:8080
+kubectl -n devpod-pro port-forward $(kubectl -n devpod-pro get pods -l app=loft -o jsonpath="{.items[0].metadata.name}") 8080:8080
--- loadtest/monitor.sh
+++ loadtest/monitor.sh
@@ -1,3 +1,16 @@
 #!/bin/zsh
 
-while true; do curl -s -v http://localhost:8080/debug/pprof/heap > $(date '+%Y-%m-%d-%H:%M:%S').out; sleep 10; done
+mkdir results
+
+export INTERVAL_SECONDS=30
+
+echo "Monitoring heap, go routines, and threads every $INTERVAL_SECONDS seconds ..."
+
+while true; do curl -s http://localhost:8080/debug/pprof/heap > ./results/$(date '+%Y-%m-%d-%H:%M:%S').heap; sleep $(echo $INTERVAL_SECONDS); done &
+
+while true; do curl -s http://localhost:8080/debug/pprof/goroutine > ./results/$(date '+%Y-%m-%d-%H:%M:%S').cpu; sleep $(echo $INTERVAL_SECONDS); done &
+
+while true; do curl -s http://localhost:8080/debug/pprof/threadcreate > ./results/$(date '+%Y-%m-%d-%H:%M:%S').threads; sleep $(echo $INTERVAL_SECONDS); done &
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+wait
