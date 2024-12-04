1917b212d1d03a8ec838e2aaaa461a0eadb6c70f
Bryan Kneis <bryan.kneis@loft.sh>
2024-12-04 07:18:19 +0000

    Fix race e2e

--- hack/build-e2e-race.sh
+++ hack/build-e2e-race.sh
@@ -12,14 +12,7 @@ then
 fi
 
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -race -ldflags "-s -w" -o $BUILD_DIR/devpod-linux-amd64 $SRC_DIR
-CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -race -ldflags "-s -w" -o $BUILD_DIR/devpod-linux-arm64 $SRC_DIR
-CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -race -ldflags "-s -w" -o $BUILD_DIR/devpod-darwin-arm64 $SRC_DIR
-CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -race -ldflags "-s -w" -o $BUILD_DIR/devpod-darwin-amd64 $SRC_DIR
 
 chmod +x $BUILD_DIR/devpod-linux-amd64
-chmod +x $BUILD_DIR/devpod-linux-arm64
-chmod +x $BUILD_DIR/devpod-darwin-arm64
-chmod +x $BUILD_DIR/devpod-darwin-amd64
 mkdir -p /tmp/devpod-cache
 cp $BUILD_DIR/devpod-linux-amd64 /tmp/devpod-cache/devpod-linux-amd64
-cp $BUILD_DIR/devpod-linux-arm64 /tmp/devpod-cache/devpod-linux-arm64
