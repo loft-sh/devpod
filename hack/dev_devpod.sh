6f16f21a8e6a764acbc1478a77217e2e45d3b4b3
Bryan Kneis <bryan.kneis@loft.sh>
2024-12-04 08:43:05 +0000

    Update dev scripts with race detector

--- hack/dev_devpod.sh
+++ hack/dev_devpod.sh
@@ -3,11 +3,18 @@
 set -e
 
 NS=${1:-"default"}
+RACE=${2:-"no"}
 
 if [[ ! $PWD == *"/go/src/devpod"* ]]; then
   echo "Please run this script from the /workspace/loft/devpod directory"
   exit 1
 fi
 
-CGO_ENABLED=1 go build -ldflags "-s -w" -tags profile -race -o devpod-cli
+if [[ $RACE == "no" ]]; then
+  CGO_ENABLED=0 go build -ldflags "-s -w" -tags profile -o devpod-cli
+else
+  echo "Building devpod with race detector"
+  CGO_ENABLED=1 go build -ldflags "-s -w" -tags profile -race -o devpod-cli
+fi
+
 kubectl -n $NS cp --no-preserve=true ./devpod-cli $(kubectl -n $NS get pods -l app=loft -o jsonpath="{.items[0].metadata.name}"):/usr/local/bin/devpod
