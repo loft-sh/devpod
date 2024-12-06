0cfd946ae10491d05804f79f97d4642c41157fdf
--global <pascal.breuninger@loft.sh>
2024-12-06 15:17:56 +0100

    chore: fix windows development setup

--- pkg/agent/inject.go
+++ pkg/agent/inject.go
@@ -81,10 +81,6 @@ func InjectAgentAndExecute(
 	versionCheck := fmt.Sprintf(`[ "$(%s version 2>/dev/null || echo 'false')" != "%s" ]`, remoteAgentPath, version.GetVersion())
 	if version.GetVersion() == version.DevVersion {
 		preferDownload = false
-
-		if runtime.GOOS == "windows" {
-			preferDownload = true
-		}
 	}
 
 	// install devpod into the target
