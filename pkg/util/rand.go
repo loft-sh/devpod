b3810f12050a02313d7a96a172fe70d09f9a9487
Bryan Kneis <bryan.kneis@loft.sh>
2024-12-18 10:41:37 +0000

    PR tidy up

---
+++ pkg/util/rand.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"math/rand"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
