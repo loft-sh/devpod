664a19d756cc337da6c580922b12840a90d36675
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-01-15 07:06:05 +0100

    chore: remove build step in devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -10,18 +10,7 @@ log() {
 # Start docker daemon. The script should've been put here by the DinD devcontainer feature
 log "Starting Docker Daemon"
 sudo /usr/local/share/docker-init.sh
-
-REBUILD_SCRIPT="./hack/rebuild.sh"
-
-if [[ ! -f "$REBUILD_SCRIPT" ]]; then
-  log "Error: Rebuild script not found at $REBUILD_SCRIPT" >&2
-  exit 1
-fi
-
-log "Building initial version of devpod binary"
-chmod +x "$REBUILD_SCRIPT"
-BUILD_PLATFORMS="linux" "$REBUILD_SCRIPT"
-
+#
 # Add our user to docker group 
 sudo usermod -aG docker $USER
 
