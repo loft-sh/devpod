bca005821f601a465418d629ee1cd013ee8c3258
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-02-20 18:38:20 +0100

    feat(desktop): windows daemon handling + shutdown

--- pkg/platform/daemon/socket.go
+++ pkg/platform/daemon/socket.go
@@ -8,24 +8,35 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/loft-sh/devpod/pkg/provider"
 )
 
-func listen(socket string) (net.Listener, error) {
-	conn, err := net.Dial("unix", socket)
+func GetSocketAddr(preferredDir, providerName string) string {
+	return filepath.Join(preferredDir, provider.DaemonSocket)
+}
+
+func dial(addr string) (net.Conn, error) {
+	return net.DialTimeout("unix", addr, 2*time.Second)
+}
+
+func listen(addr string) (net.Listener, error) {
+	conn, err := net.Dial("unix", addr)
 	if err == nil {
 		conn.Close()
-		return nil, fmt.Errorf("%s: address already in use", socket)
+		return nil, fmt.Errorf("%s: address already in use", addr)
 	}
-	_ = os.Remove(socket)
+	_ = os.Remove(addr)
 
-	sockDir := filepath.Dir(socket)
+	sockDir := filepath.Dir(addr)
 	if _, err := os.Stat(sockDir); errors.Is(err, os.ErrNotExist) {
 		_ = os.MkdirAll(sockDir, 0o755) // best effort
 	}
-	pipe, err := net.Listen("unix", socket)
+	pipe, err := net.Listen("unix", addr)
 	if err != nil {
 		return nil, err
 	}
-	_ = os.Chmod(socket, 0o666)
+	_ = os.Chmod(addr, 0o666)
 	return pipe, err
 }
