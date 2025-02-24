e89a8ed434f52d76a74029e629a0fd8c7a03d3fb
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-02-24 11:46:39 +0100

    chore: resolve dependencies

--- vendor/github.com/peterbourgon/ff/v3/ffcli/doc.go
+++
@@ -1,5 +0,0 @@
-// Package ffcli is for building declarative commandline applications.
-//
-// See the README at https://github.com/peterbourgon/ff/tree/master/ffcli
-// for more information.
-package ffcli
