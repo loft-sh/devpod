ea726f572093451db2686e9ab8de5cdc514b46f3
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-03-02 12:56:37 +0100

    fix(pro): skip update checks for internal providers

--- cmd/up.go
+++ cmd/up.go
@@ -1347,6 +1347,9 @@ func checkProviderUpdate(devPodConfig *config.Config, proInstance *provider2.Pro
 	if p.Config.Version == version.DevVersion {
 		return nil
 	}
+	if p.Config.Source.Internal {
+		return nil
+	}
 
 	v1, err := semver.Parse(strings.TrimPrefix(newVersion, "v"))
 	if err != nil {
