c0b3f1a26b8d3ce7ab27b6ff47a32b6e311537e3
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-03-03 13:21:41 +0100

    feat(pro): refactor daemonclient create and update

---
+++ pkg/client/clientimplementation/daemonclient/update.go
@@ -0,0 +1,34 @@
+package daemonclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/loft-sh/devpod/pkg/platform/form"
+	"github.com/loft-sh/log/terminal"
+)
+
+func (c *client) updateInstance(ctx context.Context) error {
+	baseClient, err := c.initPlatformClient(ctx)
+	if err != nil {
+		return err
+	}
+	if !terminal.IsTerminalIn {
+		return fmt.Errorf("unable to update instance through CLI if stdin is not a terminal")
+	}
+
+	oldInstance, err := c.localClient.GetWorkspace(ctx, c.workspace.UID)
+	if err != nil {
+		return err
+	}
+	if oldInstance == nil {
+		return fmt.Errorf("unable to find old workspace instance")
+	}
+	newInstance, err := form.UpdateInstance(ctx, baseClient, oldInstance, c.log)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.localClient.UpdateWorkspace(ctx, newInstance)
+	return err
+}
