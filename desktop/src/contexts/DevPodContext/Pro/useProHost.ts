578c8c74ff969118cfc34303f5c4694b656a351d
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-03-05 19:50:02 +0100

    fix(pro): url host

--- desktop/src/contexts/DevPodContext/Pro/useProHost.ts
+++ desktop/src/contexts/DevPodContext/Pro/useProHost.ts
@@ -1,12 +1,7 @@
-import { useMemo } from "react"
 import { useParams } from "react-router-dom"
 
 export function useProHost() {
-  const { host: urlHost } = useParams<{ host: string | undefined }>()
-
-  const host = useMemo(() => {
-    return urlHost?.replaceAll("-", ".")
-  }, [urlHost])
+  const { host } = useParams<{ host: string | undefined }>()
 
   return host
 }
