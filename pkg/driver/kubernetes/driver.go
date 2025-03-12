ac14af767471bc3180e9d45acd589538c983d100
Bryan Kneis <bryan.kneis@loft.sh>
2025-03-12 10:17:04 +0000

    Check kube config for the namespace before using the default

--- pkg/driver/kubernetes/driver.go
+++ pkg/driver/kubernetes/driver.go
@@ -15,11 +15,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// NewKubernetesDriver constructs a struct capable of provisioning a workspace and it's resources using kubernetes
 func NewKubernetesDriver(workspaceInfo *provider2.AgentWorkspaceInfo, log log.Logger) (driver.ReprovisioningDriver, error) {
 	options := workspaceInfo.Agent.Kubernetes
-	if options.KubernetesNamespace != "" {
-		log.Debugf("Use Kubernetes Namespace '%s'", options.KubernetesNamespace)
-	}
 	if options.KubernetesConfig != "" {
 		log.Debugf("Use Kubernetes Config '%s'", options.KubernetesConfig)
 	}
@@ -27,14 +25,21 @@ func NewKubernetesDriver(workspaceInfo *provider2.AgentWorkspaceInfo, log log.Lo
 		log.Debugf("Use Kubernetes Context '%s'", options.KubernetesContext)
 	}
 
-	client, err := NewClient(options.KubernetesConfig, options.KubernetesContext)
+	client, namespace, err := NewClient(options.KubernetesConfig, options.KubernetesContext)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create kubernetes client: %w", err)
+	}
+	// If the kube config has defined a namespace, use it, otherwise stick to the default namespace
+	if namespace == "" {
+		namespace = options.KubernetesNamespace
+		log.Debugf("Use Kubernetes Namespace '%s'", namespace)
+	} else {
+		log.Debugf("Use Default Kubernetes Namespace '%s'", options.KubernetesNamespace)
 	}
 
 	return &KubernetesDriver{
 		client:    client,
-		namespace: options.KubernetesNamespace,
+		namespace: namespace,
 
 		options: &options,
 		Log:     log,
