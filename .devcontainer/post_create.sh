7ae7dea02ab3203fc01180cad7a73d7ef22a5851
Jan Baraniewski <dev@baraniewski.com>
2025-03-17 13:22:27 +0100

    Update post_create script in devcontainer setup

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 set -euo pipefail
@@ -10,11 +9,14 @@ log() {
 # Start docker daemon. The script should've been put here by the DinD devcontainer feature
 log "Starting Docker Daemon"
 sudo /usr/local/share/docker-init.sh
-#
-# Add our user to docker group 
-sudo usermod -aG docker $USER
 
-log "Installing docker provider with default options"
-devpod provider add docker
+# Add devpod user to docker group
+sudo usermod -aG docker devpod
+
+# Ensure the .devpod directory is owned by devpod user
+sudo chown -R devpod:devpod /home/devpod/.devpod
+
+log "Installing docker provider with default options as devpod user"
+sudo -u devpod devpod provider add docker
 
 log "Done"
