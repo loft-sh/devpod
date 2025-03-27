831a1fff25ab3eeaeaa18eccd3009bc05ce33014
Jan Baraniewski <dev@baraniewski.com>
2025-03-27 14:16:45 +0000

    Update post-create hooks, install vcluster

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -11,10 +11,15 @@ log() {
 log "Starting Docker Daemon"
 sudo /usr/local/share/docker-init.sh
 #
-# Add our user to docker group 
+# Add our user to docker group
+sudo newgrp docker
 sudo usermod -aG docker $USER
 
 log "Installing docker provider with default options"
 devpod provider add docker
+
+
+log "Installing vCluster CLI"
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/download/v0.24.0/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
 
 log "Done"
