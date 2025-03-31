9426015d850673f06f35193e768d54f6f8caba99
Jan Baraniewski <dev@baraniewski.com>
2025-03-31 09:29:49 +0000

    Fix devcontainer DinD, install vcluster cli inside devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -11,10 +11,15 @@ log() {
 log "Starting Docker Daemon"
 sudo /usr/local/share/docker-init.sh
 #
-# Add our user to docker group 
-sudo usermod -aG docker $USER
+# Add our user to docker group
+sudo usermod -aG docker devpod
+sudo chown devpod:docker /var/run/docker.sock
 
 log "Installing docker provider with default options"
 devpod provider add docker
+
+
+log "Installing vCluster CLI"
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/download/v0.24.0/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
 
 log "Done"
