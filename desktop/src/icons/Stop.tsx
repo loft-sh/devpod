363d07700bf2d9efc42dc5934951e648d7966c79
Jan Baraniewski <dev@baraniewski.com>
2025-04-04 12:20:58 +0200

    Use stop icon in workspace card

---
+++ desktop/src/icons/Stop.tsx
@@ -0,0 +1,11 @@
+import { createIcon } from "@chakra-ui/react"
+import { defaultProps } from "./defaultProps"
+
+export const Stop = createIcon({
+  displayName: "Stop",
+  viewBox: "0 0 20 20",
+  defaultProps,
+  path: (
+    <path d="M5 5h10v10H5z" />
+  ),
+})
