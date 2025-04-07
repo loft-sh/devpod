83fb99f00ea889dd245aa5a3b8187197138f907e
bkneis <bryan.kneis@loft.sh>
2025-04-07 16:32:51 +0100

    Implement ability to pass env vars from the sandbox to host

--- desktop/src/client/command.ts
+++ desktop/src/client/command.ts
@@ -5,9 +5,11 @@ import {
   Command as ShellCommand,
 } from "@tauri-apps/plugin-shell"
 import { debug, ErrorTypeCancelled, isError, Result, ResultError, Return, sleep } from "../lib"
-import { DEVPOD_BINARY, DEVPOD_FLAG_OPTION, DEVPOD_UI_ENV_VAR } from "./constants"
+import { DEVPOD_BINARY, DEVPOD_FLAG_OPTION, DEVPOD_UI_ENV_VAR, DEVPOD_ADDITIONAL_ENV_VARS } from "./constants"
 import { TStreamEvent } from "./types"
 import { TAURI_SERVER_URL } from "./tauriClient"
+import * as log from "@tauri-apps/plugin-log"
+import { invoke } from "@tauri-apps/api/core"
 
 export type TStreamEventListenerFn = (event: TStreamEvent) => void
 export type TEventListener<TEventName extends string> = Parameters<
@@ -33,6 +35,8 @@ export class Command implements TCommand<ChildProcess<string>> {
   private childProcess?: Child
   private args: string[]
   private cancelled = false
+  private isFlatpak: boolean | undefined
+  private extraEnvVars: Record<string, string>
 
   public static ADDITIONAL_ENV_VARS: string = ""
   public static HTTP_PROXY: string = ""
@@ -41,7 +45,7 @@ export class Command implements TCommand<ChildProcess<string>> {
 
   constructor(args: string[]) {
     debug("commands", "Creating Devpod command with args: ", args)
-    const extraEnvVars = Command.ADDITIONAL_ENV_VARS.split(",")
+    this.extraEnvVars = Command.ADDITIONAL_ENV_VARS.split(",")
       .map((envVarStr) => envVarStr.split("="))
       .reduce(
         (acc, pair) => {
@@ -57,30 +61,37 @@ export class Command implements TCommand<ChildProcess<string>> {
 
     // set proxy related environment variables
     if (Command.HTTP_PROXY) {
-      extraEnvVars["HTTP_PROXY"] = Command.HTTP_PROXY
+      this.extraEnvVars["HTTP_PROXY"] = Command.HTTP_PROXY
     }
     if (Command.HTTPS_PROXY) {
-      extraEnvVars["HTTPS_PROXY"] = Command.HTTPS_PROXY
+      this.extraEnvVars["HTTPS_PROXY"] = Command.HTTPS_PROXY
     }
     if (Command.NO_PROXY) {
-      extraEnvVars["NO_PROXY"] = Command.NO_PROXY
+      this.extraEnvVars["NO_PROXY"] = Command.NO_PROXY
     }
 
     // allows the CLI to detect if commands have been invoked from the UI
-    extraEnvVars[DEVPOD_UI_ENV_VAR] = "true"
-
-    if (import.meta.env.TAURI_IS_FLATPAK === "true") {
-      this.sidecarCommand = ShellCommand.create("run-path-devpod-wrapper", args, {
-        env: { ...extraEnvVars, ["FLATPAK_ID"]: "sh.loft.devpod" },
-      })
-    } else {
-      this.sidecarCommand = ShellCommand.sidecar(DEVPOD_BINARY, args, { env: extraEnvVars })
-    }
+    this.extraEnvVars[DEVPOD_UI_ENV_VAR] = "true"
+    this.sidecarCommand = ShellCommand.sidecar(DEVPOD_BINARY, args, { env: this.extraEnvVars })
     this.args = args
+  }
+
+  public async getEnv(name: string): Promise<boolean> {
+    return invoke<boolean>("get_env", { name })
   }
 
   public async run(): Promise<Result<ChildProcess<string>>> {
     try {
+      // Run once to check with the rust backend if we are running inside the flatpak sandbox
+      // This informs the CLI wrapper to use flatpak-spawn to escape the sandbox and export this.extraEnvVars
+      if (this.isFlatpak === undefined) {
+        this.isFlatpak = await this.getEnv("FLATPAK_ID")
+        if (this.isFlatpak) {
+          this.extraEnvVars["FLATPAK_ID"] = "sh.loft.devpod"
+          this.extraEnvVars[DEVPOD_ADDITIONAL_ENV_VARS] = recordToCSV(this.extraEnvVars)
+          this.sidecarCommand = ShellCommand.sidecar(DEVPOD_BINARY, this.args, { env: this.extraEnvVars })
+        }
+      }
       const rawResult = await this.sidecarCommand.execute()
       debug("commands", `Result for command with args ${this.args}:`, rawResult)
 
@@ -231,4 +242,10 @@ export function serializeRawOptions(
   flag: string = DEVPOD_FLAG_OPTION
 ): string[] {
   return Object.entries(rawOptions).map(([key, value]) => flag + `=${key}=${value}`)
+}
+
+function recordToCSV(record: Record<string, string>): string {
+  return Object.entries(record)
+    .map(([key, value]) => `${key}=${value}`)
+    .join(',');
 }
